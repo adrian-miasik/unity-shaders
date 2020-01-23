1368f392ad1cc1572aab775a925e546110864d62
Adrian Miasik <adrianmiasik@gmail.com>
2020-01-22 23:47:35 -0500

    Create a line renderer script that visualizes our cubic bezier curve

---
+++ Assets/Scripts/LineRendererCubicBezierCurve.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AdrianMiasik;
+using UnityEngine;
+
+public class LineRendererCubicBezierCurve : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private int lineSegments = 10;
+
+    [Header("Data Points")]
+    [SerializeField] private Transform point0; // Start Anchor
+    [SerializeField] private Transform point1; // Start Handle
+    [SerializeField] private Transform point2; // End Handle
+    [SerializeField] private Transform point3; // End Anchor
+    
+    private void Reset()
+    {
+        // Quickly fetch references
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Start()
+    {
+        if (lineRenderer == null)
+        {
+            Debug.LogWarning("Missing reference to lineRenderer. (A LineRenderer Component)", gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        // Make our line renderer use X amount of segments 
+        lineRenderer.positionCount = lineSegments + 1;
+        
+        // Iterate through each segment on the line renderer
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            lineRenderer.SetPosition(i, MathUtils.GetPointOnCubicBezierCurve(
+                point0.position, 
+                point1.position, 
+                point2.position, 
+                point3.position,
+                (float) i / lineSegments
+                ));
+        }
+    }
+}
