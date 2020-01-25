582913cf78df8e63c5994e6ea46b937eea69d96a
Adrian Miasik <adrianmiasik@gmail.com>
2020-01-25 18:58:28 -0500

    Clean up LineRendererCubicBezierCurve.cs
    Place the entire file into the AdrianMiasik namespace, remove unused using directives, improve comments, improve formatting, and increase the default line segment resolution from 10 to 25.

--- Assets/Scripts/LineRendererCubicBezierCurve.cs
+++ Assets/Scripts/LineRendererCubicBezierCurve.cs
@@ -1,49 +1,49 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using AdrianMiasik;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class LineRendererCubicBezierCurve : MonoBehaviour
+namespace AdrianMiasik
 {
-    [SerializeField] private LineRenderer lineRenderer;
-    [SerializeField] private int lineSegments = 10;
-
-    [Header("Data Points")]
-    [SerializeField] private Transform point0; // Start Anchor
-    [SerializeField] private Transform point1; // Start Handle
-    [SerializeField] private Transform point2; // End Handle
-    [SerializeField] private Transform point3; // End Anchor
-    
-    private void Reset()
+    [ExecuteInEditMode]
+    public class LineRendererCubicBezierCurve : MonoBehaviour
     {
-        // Quickly fetch references
-        lineRenderer = GetComponent<LineRenderer>();
-    }
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private int lineSegments = 25;
 
-    private void Start()
-    {
-        if (lineRenderer == null)
+        [Header("Target Points")]
+        [SerializeField] private Transform point0; // Start Anchor
+        [SerializeField] private Transform point1; // Start Handle
+        [SerializeField] private Transform point2; // End Handle
+        [SerializeField] private Transform point3; // End Anchor
+
+        private void Reset()
         {
-            Debug.LogWarning("Missing reference to lineRenderer. (A LineRenderer Component)", gameObject);
+            // Quickly fetch references
+            lineRenderer = GetComponent<LineRenderer>();
         }
-    }
 
-    private void Update()
-    {
-        // Make our line renderer use X amount of segments 
-        lineRenderer.positionCount = lineSegments + 1;
-        
-        // Iterate through each segment on the line renderer
-        for (int i = 0; i < lineRenderer.positionCount; i++)
+        private void Start()
         {
-            lineRenderer.SetPosition(i, MathUtils.GetPointOnCubicBezierCurve(
-                point0.position, 
-                point1.position, 
-                point2.position, 
-                point3.position,
-                (float) i / lineSegments
-                ));
+            if (lineRenderer == null)
+            {
+                Debug.LogWarning("Missing reference to lineRenderer. (A LineRenderer Component)", gameObject);
+            }
+        }
+
+        private void Update()
+        {
+            // Define the length of our lineRenderer positions
+            lineRenderer.positionCount = lineSegments + 1; // Plus one because two points make up one segment
+
+            // Iterate through each segment on the line renderer
+            for (int i = 0; i < lineRenderer.positionCount; i++)
+            {
+                // Move each line renderer point to where they need to be on the bezier curve
+                lineRenderer.SetPosition(i, MathUtils.GetPointOnCubicBezierCurve(
+                    point0.position, 
+                    point1.position,
+                    point2.position, 
+                    point3.position,
+                    (float) i / lineSegments));
+            }
         }
     }
 }
