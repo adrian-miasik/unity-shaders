d5964d403f4b650051eec5628e7f3072d902cb4f
Adrian Miasik <adrianmiasik@gmail.com>
2020-01-26 23:26:34 -0500

    Create OpenURLOnClick.cs

---
+++ Assets/Scripts/OpenURLOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace AdrianMiasik
+{
+    public class OpenURLOnClick : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private string url;
+        
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Application.OpenURL(url);
+        }
+    }
+}
