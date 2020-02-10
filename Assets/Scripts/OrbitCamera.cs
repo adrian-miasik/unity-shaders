2b79dc48100e36d25ce601b72d306c408a209ce7
Adrian Miasik <adrianmiasik@gmail.com>
2020-02-09 21:05:54 -0500

    Modify orbit camera to support mouse freelook, auto spin, and zoom functionality

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -3,12 +3,21 @@ using UnityEngine;
 
 namespace AdrianMiasik
 {
+    [ExecuteInEditMode]
     public class OrbitCamera : MonoBehaviour
     {
         [SerializeField] private Camera camera;
         [SerializeField] private Transform cameraTarget;
-        [SerializeField] private float distanceFromTarget = 15f;
+        [SerializeField] private Vector3 positionOffset;
+        [SerializeField] private Vector2 startingRotation = new Vector2(45f,0f);
         [SerializeField] private Vector2 axisSensitivity = Vector2.one;
+        [SerializeField] private float distanceFromTarget = 15f;
+        
+        
+        
+        [SerializeField] private bool autoSpin;
+        [SerializeField] private Vector2 spinDirection = Vector2.up;
+        [SerializeField] private float spinSpeed = 0.1f;
         
         private Vector3 zoom;
         private Quaternion rotation;
@@ -40,21 +49,41 @@ namespace AdrianMiasik
             resultVector += FetchInput() * axisSensitivity;
             
             // Translate & rotate camera
-            MoveCamera(resultVector);
+            MoveCamera(resultVector + startingRotation);
         }
 
         private Vector2 FetchInput()
         {
-            return new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+            Vector2 result = Vector2.zero;
+            
+            result += new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"));
+            
+            if (Input.GetMouseButton(1))
+            {
+                result += new Vector2(Input.GetAxis("Mouse Y") * -1, Input.GetAxis("Mouse X") * -1);
+            }
+
+            if (autoSpin)
+            {
+                result += spinDirection * spinSpeed;
+            }
+
+            return result;
         }
         
         private void MoveCamera(Vector2 _inputVector)
         {
             rotation = Quaternion.Euler(_inputVector.x, -_inputVector.y, 0);
             zoom = new Vector3(0,0, -distanceFromTarget); // Top down camera
-            
-            camera.transform.position = rotation * zoom + cameraTarget.transform.position;
-            camera.transform.LookAt(cameraTarget);
+
+            Vector3 targetPoint = cameraTarget.transform.position + positionOffset;
+            camera.transform.position = rotation * zoom + targetPoint;
+            camera.transform.LookAt(targetPoint);
+        }
+
+        public void SetZoomDistance(float _desiredZoom)
+        {
+            distanceFromTarget = _desiredZoom;
         }
     }
 }
