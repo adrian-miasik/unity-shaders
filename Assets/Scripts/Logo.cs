209f6770951b0d8b8c2b30ae6504ff00a152cd3a
Adrian Miasik <adrianmiasik@gmail.com>
2020-02-16 23:48:18 -0500

    Fix logo animation
    Missing script re-implemented

---
+++ Assets/Scripts/Logo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Logo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Animator animator;
+    
+    private static readonly int HoverEnter = Animator.StringToHash("HoverEnter");
+    private static readonly int HoverExit = Animator.StringToHash("HoverExit");
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        animator.SetTrigger(HoverEnter);
+    }
+    
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        animator.SetTrigger(HoverExit);
+    }
+}
