57849a3f13309b5d0bd5075c246baa1f95c56772
Adrian Miasik <adrianmiasik@gmail.com>
2020-02-27 23:41:42 -0500

    Create and implement disappearing cow shader

---
+++ Assets/AutoPlayDisappearingCow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPlayDisappearingCow : MonoBehaviour
+{
+    [SerializeField] private Material disappearingCowMaterial;
+    [SerializeField] private float progressDuration = 3f;
+    private float progress;
+    
+    private float elapsedTime;
+    
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        progress = elapsedTime / progressDuration;
+        disappearingCowMaterial.SetFloat("Vector1_19CF4714", progress);
+        
+        if (elapsedTime >= progressDuration)
+        {
+            elapsedTime = 0;
+        }
+    }
+}
