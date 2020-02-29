1c99adc0574448e9c733450eeb9bd8c5ef58c175
Adrian Miasik <adrianmiasik@gmail.com>
2020-02-29 17:44:48 -0500

    Auto play disappearing cow script now uses material property blocks
    This way each renderer can have their own shader values while using the same material and shader. (No need for a new material / material instance)

--- Assets/AutoPlayDisappearingCow.cs
+++ Assets/AutoPlayDisappearingCow.cs
@@ -3,23 +3,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class AutoPlayDisappearingCow : MonoBehaviour
 {
-    [SerializeField] private Material disappearingCowMaterial;
+    [SerializeField] private Renderer rend;
     [SerializeField] private float progressDuration = 3f;
     private float progress;
+    private MaterialPropertyBlock materialBlock;
     
     private float elapsedTime;
-    
+    private static readonly int Vector119Cf4714 = Shader.PropertyToID("Vector1_19CF4714");
+
+    private void Start()
+    {
+        materialBlock = new MaterialPropertyBlock();
+    }
+
     private void Update()
     {
         elapsedTime += Time.deltaTime;
-        progress = elapsedTime / progressDuration;
-        disappearingCowMaterial.SetFloat("Vector1_19CF4714", progress);
+        
+        SetRendererProgress(elapsedTime / progressDuration);
         
         if (elapsedTime >= progressDuration)
         {
             elapsedTime = 0;
         }
+    }
+
+    private void SetRendererProgress(float _progress)
+    {
+        materialBlock.SetFloat(Vector119Cf4714, _progress);
+        rend.SetPropertyBlock(materialBlock);
     }
 }
