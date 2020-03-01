3f45ef7f1194a3df8903d4b8726eda07ddc3698a
Adrian Miasik <adrianmiasik@gmail.com>
2020-02-29 20:51:08 -0500

    Create a timer progress abstract class and convert auto play cow shader script to generic shader property script

---
+++ Assets/Scripts/ShaderTimerProgress.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace AdrianMiasik
+{
+    public class ShaderTimerProgress : TimerProgress
+    {
+        [SerializeField] private Renderer rend;
+        [SerializeField] private string shaderProgressProperty;
+        
+        private MaterialPropertyBlock materialBlock;
+        private int shaderProgressID;
+        
+        protected override bool Initialize()
+        {
+            shaderProgressID = Shader.PropertyToID(shaderProgressProperty);
+            materialBlock = new MaterialPropertyBlock();
+            
+            return true;
+        }
+        
+        protected override void OnUpdate(float _progress)
+        {
+            SetRendererProgress(_progress);
+        }
+
+        protected override void OnComplete()
+        {
+            // Nothing
+        }
+
+        private void SetRendererProgress(float _progress)
+        {
+            materialBlock.SetFloat(shaderProgressID, _progress);
+            rend.SetPropertyBlock(materialBlock);
+        }
+    }
+}
