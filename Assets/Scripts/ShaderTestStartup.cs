08abdb38e09aa1bc234700956bed9542225f59bf
Adrian Miasik <adrianmiasik@gmail.com>
2020-03-31 00:17:24 -0400

    Create a scene start up script for ShaderTest.unity

---
+++ Assets/Scripts/ShaderTestStartup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace AdrianMiasik{
+    public class ShaderTestStartup : MonoBehaviour
+    {
+        [SerializeField] private DisplayCaseCarousel standardCarousel;
+        [SerializeField] private DisplayCaseCarousel shaderGraphCarousel;
+
+        private void Start()
+        {
+            SetupEnvironment();
+        }
+
+        [ContextMenu("Setup Environment")]
+        private void SetupEnvironment()
+        {
+            standardCarousel.Initialize();
+            shaderGraphCarousel.Initialize();
+        }
+    }
+}
