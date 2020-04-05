9f0666648cc7283caf58451716a24e3f624d4d80
Adrian Miasik <adrianmiasik@gmail.com>
2020-04-05 19:07:34 -0400

    Code clean up - UIContextManager.cs

--- Assets/Scripts/UIContextManager.cs
+++ Assets/Scripts/UIContextManager.cs
@@ -6,13 +6,13 @@ namespace AdrianMiasik
     public class UIContextManager : MonoBehaviour
     {
         [SerializeField] private TMP_Text text = null;
-        [SerializeField] private DisplayCaseCarousel _selector = null;
+        [SerializeField] private DisplayCaseCarousel selector = null;
 
         public void Start()
         {
-            _selector.onDisplayChange += OnSelectionChange;
+            selector.onDisplayChange += OnSelectionChange;
 
-            text.text = _selector.GetSelectedDisplayModel().GetModelRenderer().sharedMaterial.shader.ToString();
+            text.text = selector.GetSelectedDisplayModel().GetModelRenderer().sharedMaterial.shader.ToString();
         }
         
         private void OnSelectionChange(DisplayCase _previousCase, DisplayCase _currentCase)
