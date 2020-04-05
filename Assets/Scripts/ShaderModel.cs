311471e4cc65ff632e5b2fdab35e8e836d3cfa38
Adrian Miasik <adrianmiasik@gmail.com>
2020-04-05 05:42:14 -0400

    Create a shader model script - Used to initialize certain components (Just a simple hover for now)

---
+++ Assets/Scripts/ShaderModel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace AdrianMiasik
+{
+    public class ShaderModel : MonoBehaviour
+    {
+        [Header("Optional")]
+        [SerializeField] private Hover hover = null;
+            
+        public void Initialize()
+        {
+            if (hover != null)
+            {
+                hover.Initialize();
+            }
+        }
+    }
+}
