c20b480232a1eeb367541a5e602c8ef0b5e57924
Adrian Miasik <adrianmiasik@gmail.com>
2020-04-05 19:08:34 -0400

    Code clean up - GameObjectActiveSelector.cs

--- Assets/Scripts/Selector/GameObjectActiveSelector.cs
+++ Assets/Scripts/Selector/GameObjectActiveSelector.cs
@@ -4,15 +4,15 @@ namespace AdrianMiasik
 {
     public class GameObjectActiveSelector : MonoBehaviour
     {
-        [SerializeField] private ItemSelector<GameObject> _selector = null;
+        [SerializeField] private ItemSelector<GameObject> gameObjectSelector = null;
 
         private void Start()
         {
-            _selector.Initialize();
-            _selector.onSelectionChange += OnSelectionChange;
+            gameObjectSelector.Initialize();
+            gameObjectSelector.onSelectionChange += OnSelectionChange;
         }
         
-        private void OnSelectionChange(GameObject _previousSelection, GameObject _currentSelection)
+        private static void OnSelectionChange(GameObject _previousSelection, GameObject _currentSelection)
         {
             _previousSelection.SetActive(false);
             _currentSelection.SetActive(true);
