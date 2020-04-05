2f7e9f60bc0e900f011c6ab921b942697f375d71
Adrian Miasik <adrianmiasik@gmail.com>
2020-04-05 05:38:42 -0400

    Create a hover script - This oscillates gameobjects vertically (up and down) using a sine wave

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace AdrianMiasik
+{
+    public class Hover : MonoBehaviour
+    {
+        [SerializeField] private float amplitude;
+        [SerializeField] private float heightOffset = 0;
+        [SerializeField] private float speed = 1f;
+
+        private float accumulatedTime;
+        private Vector3 startingPosition;
+        private Vector3 targetPosition;
+        private bool isInitialized = false;
+
+        public void Initialize()
+        {
+            startingPosition = transform.localPosition;
+            isInitialized = true;
+        }
+
+        private void Update()
+        {
+            if (!isInitialized)
+            {
+                return;
+            }
+
+            accumulatedTime += Time.deltaTime;
+            
+            targetPosition = startingPosition + (Vector3.up * heightOffset);
+            transform.localPosition = targetPosition + (Vector3.up * (Mathf.Sin(accumulatedTime * speed) * amplitude));
+        }
+    }
+}
