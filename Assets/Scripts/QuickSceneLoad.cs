4468e6f6601e347411216f5eaa973fec19b62aa4
Adrian Miasik <adrianmiasik@gmail.com>
2020-05-19 23:52:07 -0400

    Create a quick scene reload script

---
+++ Assets/Scripts/QuickSceneLoad.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AdrianMiasik
+{
+    public class QuickSceneLoad : MonoBehaviour
+    {
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F5))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
