f4975aaa22d5cc1132c87c75fddcdb2436a1742f
Adrian Miasik <adrianmiasik@gmail.com>
2020-05-20 00:36:39 -0400

    Fix warning in logo script

--- Assets/Scripts/AdrianMiasikLogo.cs
+++ Assets/Scripts/AdrianMiasikLogo.cs
@@ -5,7 +5,7 @@ namespace AdrianMiasik
 {
     public class AdrianMiasikLogo : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        [SerializeField] private Animator animator;
+        [SerializeField] private Animator animator = null;
         private static readonly int HoverEnter = Animator.StringToHash("HoverEnter");
         private static readonly int HoverExit = Animator.StringToHash("HoverExit");
 
