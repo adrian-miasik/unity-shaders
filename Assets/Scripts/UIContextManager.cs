f060944fc5e1b7d936415a650a2a2b9da5400c99
Adrian Miasik <AdrianMiasik@gmail.com>
2020-09-08 01:46:17 -0400

    Implement context manager element - New UI and supports dynamically sized label.
    If the text extends past the bounds of the 'UI Context' width, we will fit the text and force it within the label. Otherwise we display it using our preferred text size and scale the label to conform to the font.
    
    Basically if the content is too big, we scale it down. Otherwise we scale it to it's preferred width and size.

--- Assets/Scripts/UIContextManager.cs
+++ Assets/Scripts/UIContextManager.cs
@@ -1,14 +1,49 @@
 ﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace AdrianMiasik
 {
     public class UIContextManager : MonoBehaviour
     {
+        [SerializeField] private RectTransform container = null;
+        [SerializeField] private ContentSizeFitter contentFitter = null;
         [SerializeField] private TMP_Text text = null;
         [SerializeField] private List<DisplayCaseCarousel> selector = null;
+        [SerializeField] private int preferredFontSize = 38;
+        
+        private void Refresh()
+        {
+            if (text.text == string.Empty)
+            {
+                contentFitter.horizontalFit = ContentSizeFitter.FitMode.MinSize;
+            }
+            
+            // Change fit and update ui elements (font before layout group)
+            text.enableAutoSizing = false;
+            text.fontSize = preferredFontSize;
+            contentFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(container);
 
+            Debug.LogWarning(container.sizeDelta.x);
+            
+            // if content is overflowing...
+            if (container.sizeDelta.x > 0)
+            {
+                contentFitter.horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
+                container.Resize();
+                text.enableAutoSizing = true;
+            }
+            // else content is within bounds...
+            else
+            {
+                contentFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+                text.enableAutoSizing = false;
+                text.fontSize = preferredFontSize;
+            }
+        }
+        
         public void Start()
         {
             foreach (DisplayCaseCarousel _carousel in selector)
@@ -16,6 +51,8 @@ namespace AdrianMiasik
                 _carousel.onDisplayChange += OnSelectionChange;
                 text.text = _carousel.GetSelectedDisplayModel().GetModelRenderer().sharedMaterial.shader.ToString();
             }
+            
+            Refresh();
         }
         
         private void OnSelectionChange(DisplayCase _previousCase, DisplayCase _currentCase)
@@ -28,6 +65,7 @@ namespace AdrianMiasik
 
             text.enabled = true;
             text.text = _currentCase.GetModelRenderer().sharedMaterial.shader.ToString();
+            Refresh();
         }
     }
 }
