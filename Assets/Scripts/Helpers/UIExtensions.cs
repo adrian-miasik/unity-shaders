c2e7a091ab7b837544264a0b96049a036c56f3d5
Adrian Miasik <AdrianMiasik@gmail.com>
2020-09-08 01:40:50 -0400

    Create a UIExtensions helper script - specifically for extension methods related to UI

---
+++ Assets/Scripts/Helpers/UIExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace AdrianMiasik
+{
+    public static class UIExtensions
+    {
+        public static void Resize(this RectTransform _rectTransform)
+        {
+            _rectTransform.anchorMin = Vector2.zero;
+            _rectTransform.anchorMax = Vector2.one;
+            _rectTransform.offsetMin = Vector2.zero;
+            _rectTransform.offsetMax = Vector2.zero;
+        }
+    }
+}
