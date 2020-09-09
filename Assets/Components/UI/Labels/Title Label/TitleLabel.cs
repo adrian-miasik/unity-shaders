1e2dd4d6a9c6c069bb8c21a4d7979a2571200959
Adrian Miasik <AdrianMiasik@gmail.com>
2020-09-09 00:22:36 -0400

    Create a title label component (ui context menu replacement)

---
+++ Assets/Components/UI/Labels/Title Label/TitleLabel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AdrianMiasik
+{
+    public class TitleLabel : MonoBehaviour
+    {
+        [SerializeField] private RectTransform container = null;
+        [SerializeField] private ContentSizeFitter contentFitter = null;
+        [SerializeField] private TMP_Text text = null;
+        [SerializeField] private int preferredFontSize = 38;
+
+        public void Initialize(string _message)
+        {
+            text.text = _message;
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            if (text.text == string.Empty)
+            {
+                contentFitter.horizontalFit = ContentSizeFitter.FitMode.MinSize;
+            }
+            
+            // Change fit and update ui elements (font before layout group)
+            text.enableAutoSizing = false;
+            text.fontSize = preferredFontSize;
+            contentFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(container);
+            
+            // if content is overflowing...
+            if (container.sizeDelta.x > 0)
+            {
+                Debug.Log("Scale to fit.", gameObject);
+                contentFitter.horizontalFit = ContentSizeFitter.FitMode.Unconstrained;
+                container.Resize();
+                text.enableAutoSizing = true;
+            }
+            // else content is within bounds...
+            else
+            {
+                contentFitter.horizontalFit = ContentSizeFitter.FitMode.PreferredSize;
+                text.enableAutoSizing = false;
+                text.fontSize = preferredFontSize;
+            }
+        }
+        
+        public void Hide()
+        {
+            Debug.LogWarning("TODO: Animate context label out");
+            Destroy(gameObject);
+        }
+    }
+}
